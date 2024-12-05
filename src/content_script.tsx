b3c943ba1b471cd4794f18a4a38f61dc43843418
Matt Wilkinson <matwilmail@gmail.com>
2024-12-05 21:24:48 +0000

    apply styles to enable responsiveness

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -24,13 +24,37 @@ const modalContentStyle =
 `
   background-color: #fefefe;
   margin: 15% auto;
-  padding: 20px;
   border: 1px solid #888;
-  width: 650px;
+  width: 100%;
   height: 60%;
   position: relative;
   overflow: hidden;
 `
+
+const style = document.createElement('style');
+style.type = 'text/css';
+
+// Add responsive CSS rules
+style.innerHTML = `
+  .modalContent {
+    width: 100%;
+    background-color: #fefefe;
+    margin: 15% auto;
+    border: 1px solid #888;
+    height: 60%;
+    position: relative;
+    overflow: hidden;
+  }
+  
+  @media (min-width: 600px) {
+    .modalContent {
+      width: 600px;
+    }
+  }
+`;
+
+// Append the <style> element to the <head>
+document.head.appendChild(style);
 
 // Add clickable Street View icon and popup to each listing
 listings.forEach((listing, index) => {
@@ -49,8 +73,9 @@ listings.forEach((listing, index) => {
   popup.id = `popup-${id}`
   popup.style.cssText = modalStyle
   const content = document.createElement("div");
-  content.style.cssText = modalContentStyle
-  content.innerHTML = `<iframe src="https://www.rightmove.co.uk/properties/${id}#/streetView?channel=RES_BUY" width="600" height="450" style="border:0;position:absolute;top:-40px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+  content.className = "modalContent"
+
+  content.innerHTML = `<iframe id="mapframe" src="https://www.rightmove.co.uk/properties/${id}#/streetView?channel=RES_BUY" width="100%" height="450" style="border:0;position:absolute;top:-40px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
   popup.appendChild(content)
   listing.appendChild(popup)
 
