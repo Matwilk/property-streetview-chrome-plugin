14c2bb254164901c760bb7bdcc30a5a1562c86bc
Matt Wilkinson <matwilmail@gmail.com>
2024-12-30 13:56:48 +0000

    remove background worker

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -15,9 +15,6 @@ const envKeys = Object.keys(env).reduce((prev, next) => {
 
 module.exports = {
     entry: {
-    //   popup: path.join(srcDir, 'popup.tsx'),
-    //   options: path.join(srcDir, 'options.tsx'),
-      background: path.join(srcDir, 'background.ts'),
       content_script: path.join(srcDir, 'content_script.tsx'),
     },
     output: {
